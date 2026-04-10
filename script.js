7c34d15523c8433779282b320878ed3458e88821
Penélope Vieira <vieirapenelope21@gmail.com>
2026-04-10 01:39:22 -0300

    Fix comment typo and add form processing logic

--- script.js
+++ script.js
@@ -2,5 +2,24 @@
 
 
 
-/* Validação e confirmação de formulção */
+/* Validação e confirmação de formulário */
 
+const form = document.querySelector('form');
+
+function processarReserva(evento) {
+    evento.preventDefault();
+
+    const inpNome = document.getElementById('nome');
+    const inpEmail = document.getElementById('email');
+    const spam = document.querySelector('input[type="checkbox"]');
+
+    
+    if (spam.checked && inpEmail.value === "") {
+        alert("Por favor, preencha seu e-mail para receber nossas novidades!");
+    } else {
+        alert("Obrigado, " + inpNome.value + "! Sua reserva foi enviada.");
+        form.reset();
+    }
+}
+
+form.addEventListener('submit', processarReserva);
